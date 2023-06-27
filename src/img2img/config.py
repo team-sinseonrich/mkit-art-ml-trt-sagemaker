332e8c126ce670a8891557249cc9cf68476ee9c3
richardSHkim <kshtrue95@gmail.com>
2023-06-27 11:30:55 +0000

    [MA-59] feat(inference): create config file

---
+++ src/img2img/config.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+import groundingdino.datasets.transforms as T
+
+
+@dataclass
+class Config:
+    # Env
+    device = "cuda"
+    
+    # GroundingDINO
+    ckpt_repo_id = "ShilongLiu/GroundingDINO"
+    ckpt_filename = "groundingdino_swinb_cogcoor.pth"
+    ckpt_config_filename = "GroundingDINO_SwinB.cfg.py"
+    cache_config_file  = "../models/models--ShilongLiu--GroundingDINO/snapshots/a94c9b567a2a374598f05c584e96798a170c56fb/GroundingDINO_SwinB.cfg.py"
+    cache_file = "../models/models--ShilongLiu--GroundingDINO/snapshots/a94c9b567a2a374598f05c584e96798a170c56fb/groundingdino_swinb_cogcoor.pth"
+    transform = T.Compose(
+        [
+            T.RandomResize([800], max_size=1333),
+            T.ToTensor(),
+            T.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]),
+        ]
+    )
+
+    # SAM
+    sam_checkpoint = "../models/sam_vit_h_4b8939.pth"
+    model_type = "vit_h"
+
+cfg = Config()
